05ceb0520d15712e0da101aaa393b5cc0338f5f8
Virviglaz <Virviglaz@users.noreply.github.com>
2024-02-24 20:47:05 +0000

    deploy: cc9e002b9c1b82aea6c54d3302b8e0e91b079df0

--- search/classes_1.js
+++ search/classes_1.js
@@ -6,7 +6,6 @@ var searchData=
   ['can_5ftypedef_3',['CAN_TypeDef',['../struct_c_a_n___type_def.html',1,'']]],
   ['cec_5ftypedef_4',['CEC_TypeDef',['../struct_c_e_c___type_def.html',1,'']]],
   ['comp_5ftypedef_5',['COMP_TypeDef',['../struct_c_o_m_p___type_def.html',1,'']]],
-  ['cr2_5freg_6',['cr2_reg',['../structcr2__reg.html',1,'']]],
-  ['crc_5ftypedef_7',['CRC_TypeDef',['../struct_c_r_c___type_def.html',1,'']]],
-  ['crs_5ftypedef_8',['CRS_TypeDef',['../struct_c_r_s___type_def.html',1,'']]]
+  ['crc_5ftypedef_6',['CRC_TypeDef',['../struct_c_r_c___type_def.html',1,'']]],
+  ['crs_5ftypedef_7',['CRS_TypeDef',['../struct_c_r_s___type_def.html',1,'']]]
 ];
--- search/classes_6.js
+++ search/classes_6.js
@@ -1,8 +1,5 @@
 var searchData=
 [
-  ['i2c_5fdev_0',['i2c_dev',['../structi2c__dev.html',1,'']]],
-  ['i2c_5fmsg_5ft_1',['i2c_msg_t',['../structi2c__msg__t.html',1,'']]],
-  ['i2c_5ftypedef_2',['I2C_TypeDef',['../struct_i2_c___type_def.html',1,'']]],
-  ['i2c_5fxfer_3',['i2c_xfer',['../structi2c__xfer.html',1,'']]],
-  ['iwdg_5ftypedef_4',['IWDG_TypeDef',['../struct_i_w_d_g___type_def.html',1,'']]]
+  ['i2c_5ftypedef_0',['I2C_TypeDef',['../struct_i2_c___type_def.html',1,'']]],
+  ['iwdg_5ftypedef_1',['IWDG_TypeDef',['../struct_i_w_d_g___type_def.html',1,'']]]
 ];
